b8889a5939d8bc51da15437ea3563d2b7da61dc3
John Grattan <john.grattan@ienglish.com>
2013-01-08 10:08:54 +0900

    Initial Commit

---
+++ GitTestNibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTestNibs
+//
+//  Created by John Grattan on 13/8/1.
+//  Copyright (c) 2013 John Grattan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ GitTestNibs/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  GitTestNibs
+//
+//  Created by John Grattan on 13/8/1.
+//  Copyright (c) 2013 John Grattan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
